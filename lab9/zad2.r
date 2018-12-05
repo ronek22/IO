dfb1c8d86cb946f7c41f56c9df6b9bcf6c11f557
ronek22 <jronek3010@gmail.com>
2018-12-05 12:36:33 +0100

    Finished second exercise

--- lab9/zad2.r
+++ lab9/zad2.r
@@ -10,12 +10,22 @@ iris.norm$Species = iris$Species
 iris.norm$Setosa = iris.norm$Species == "setosa"
 iris.norm$Virginica = iris.norm$Species == "virginica"
 iris.norm$Versicolor = iris.norm$Species == "versicolor"
-iris.norm$Species <- NULL
-#B
+# B
 set.seed(1234)
 ind <- sample(2, nrow(iris.norm), replace=TRUE, prob=c(0.67, 0.33))
-trainSet <-iris.norm[ind==1, 1:7]
-testSet <-iris.norm[ind==2, 1:7]
+trainSet <-iris.norm[ind==1, 1:8]
+testSet <-iris.norm[ind==2, 1:8]
 
-
+# C
 neuralIris <- neuralnet(Setosa + Versicolor + Virginica ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, trainSet, hidden=4)
+plot(neuralIris)
+
+# D
+pr <- compute(neuralIris, testSet[,1:4])
+result <- as.data.frame(pr$net.result)
+colnames(result) <- c("Setosa", "Versicolor", "Virginica")
+
+predicted <- colnames(result)[max.col(result,ties.method="first")]
+real <- testSet$Species
+
+conf.matrix <- table(predicted, real)
